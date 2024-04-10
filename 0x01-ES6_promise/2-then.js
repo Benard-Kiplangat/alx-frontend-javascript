c81a2ef633dc0423e9bef41d4e9cc00a6037ce54
benard Kiplangat <benardkiplangat97@gmail.com>
2024-04-10 23:33:59 +0300

    then on promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,4 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(function () { console.log('Got a response from the API'); return { status: 200, body: 'Success', };
+}, function () { return Error('') })
+}
