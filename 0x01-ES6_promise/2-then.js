3d1f1f690f9e76ce080bf00f7b7e22c6e0d14f7d
benard Kiplangat <benardkiplangat97@gmail.com>
2024-04-11 00:05:04 +0300

    modified:   0x01-ES6_promise/2-then.js 	modified:   0x01-ES6_promise/3-all.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,4 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(function () { console.log('Got a response from the API'); return { status: 200, body: 'Success', };
-}, function () { return Error('') })
+  promise.then(() => {
+    console.log('Got a response from the API'); return { status: 200, body: 'Success' };
+  }, () => Error(''));
 }
---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,5 @@
+import { createUser, uploadPhoto } from "./utils";
+
+function handleProfileSignup() {
+    console.log(uploadPhoto.body, createUser.firstName, createUser.lastName)
+}
